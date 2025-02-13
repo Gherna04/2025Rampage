191557d31fa18e3e1204a1472680ee40e4a2258e
BionicOcto <Gherna105@gmail.com>
2025-02-13 16:53:24 -0700

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -20,8 +20,6 @@ public class Robot extends TimedRobot {
   
       private static final String fullClassName = MethodHandles.lookup().lookupClass().getCanonicalName();
 
-    // *** STATIC INITIALIZATION BLOCK ***
-    // This block of code is run first when the class is loaded
     static
     {
         System.out.println("Loading: " + fullClassName);

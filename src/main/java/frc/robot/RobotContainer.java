7247b03760d489e2a16dde677578db1fbec3303f
BionicOcto <Gherna105@gmail.com>
2025-03-10 16:53:42 -0600

    Add files via upload

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,227 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
+//import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.swervedrive.drivebase.AbsoluteDriveAdv;
+import frc.robot.subsystems.SwerveSubsystem;
+import java.io.File;
+import swervelib.SwerveInputStream;
+
+// Subsystems and commands
+import frc.robot.subsystems.ElavatorSubsystem;
+import frc.robot.commands.swervedrive.drivebase.ElavatorCommand;
+import frc.robot.commands.swervedrive.drivebase.SetElavatorPositionCommand;
+
+public class RobotContainer
+{
+  // -------------------------------
+  // Controllers
+  // -------------------------------
+  final CommandPS5Controller driverPs5Controller   = new CommandPS5Controller(0);
+  final CommandPS5Controller operatorPs5Controller = new CommandPS5Controller(1);
+  final CommandPS5Controller buttonBoard  = new CommandPS5Controller(2);
+
+  // POV (D-pad) buttons for operator
+  private final POVButton dpadUpButton    = new POVButton(driverPs5Controller.getHID(), 0);
+  private final POVButton dpadRightButton = new POVButton(driverPs5Controller.getHID(), 90);
+  private final POVButton dpadDownButton  = new POVButton(driverPs5Controller.getHID(), 180);
+  //private final POVButton dpadLeftButton  = new POVButton(operatorPS5Controller.getHID(), 270);
+
+  // -------------------------------
+  // Subsystems
+  // -------------------------------
+  private final SwerveSubsystem drivebase = new SwerveSubsystem(
+      new File(Filesystem.getDeployDirectory(), "swerve")
+  );
+
+
+  private final ElavatorSubsystem ElavatorSubsystem = new ElavatorSubsystem();
+
+
+  // -------------------------------
+  // Drive commands
+  // -------------------------------
+  AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(
+      drivebase,
+      () -> -MathUtil.applyDeadband(driverPs5Controller.getLeftY(), OperatorConstants.LEFT_Y_DEADBAND),
+      () -> -MathUtil.applyDeadband(driverPs5Controller.getLeftX(), OperatorConstants.DEADBAND),
+      () -> -MathUtil.applyDeadband(driverPs5Controller.getRightX(), OperatorConstants.RIGHT_X_DEADBAND),
+      driverPs5Controller.getHID()::getSquareButtonPressed,
+      driverPs5Controller.getHID()::getCircleButtonPressed,
+      driverPs5Controller.getHID()::getTriangleButtonPressed,
+      driverPs5Controller.getHID()::getCrossButtonPressed
+  );
+
+  // Convert driver input to field-relative controls
+  SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
+      drivebase.getSwerveDrive(),
+      () -> driverPs5Controller.getLeftY() * -1,
+      () -> driverPs5Controller.getLeftX() * -1
+  )
+  .withControllerRotationAxis(driverPs5Controller::getRightX)
+  .deadband(OperatorConstants.DEADBAND)
+  .scaleTranslation(0.8)
+  .allianceRelativeControl(true)
+  .translationHeadingOffset(true)
+  .translationHeadingOffset(drivebase.getHeading());
+
+  // Field-relative input stream
+  SwerveInputStream driveDirectAngle =
+      driveAngularVelocity.copy()
+                          .withControllerHeadingAxis(
+                              () -> driverPs5Controller.getRightX() * -1,
+                              () -> driverPs5Controller.getRightY() * -1
+                          )
+                          .headingWhile(true);
+
+  // Default drive commands
+  Command driveFieldOrientedDirectAngle      = drivebase.driveFieldOriented(driveDirectAngle);
+  Command driveFieldOrientedAnglularVelocity = drivebase.driveFieldOriented(driveAngularVelocity);
+  Command driveSetpointGen                  = drivebase.driveWithSetpointGeneratorFieldRelative(driveDirectAngle);
+
+  // Sim-specific
+  SwerveInputStream driveAngularVelocitySim = SwerveInputStream.of(
+      drivebase.getSwerveDrive(),
+      () -> -driverPs5Controller.getLeftY(),
+      () -> -driverPs5Controller.getLeftX()
+  )
+  .withControllerRotationAxis(() -> driverPs5Controller.getRawAxis(2))
+  .deadband(OperatorConstants.DEADBAND)
+  .scaleTranslation(0.8)
+  .allianceRelativeControl(true);
+
+  SwerveInputStream driveDirectAngleSim =
+      driveAngularVelocitySim.copy()
+                             .withControllerHeadingAxis(
+                                 () -> Math.sin(driverPs5Controller.getRawAxis(2) * Math.PI) * (Math.PI * 2),
+                                 () -> Math.cos(driverPs5Controller.getRawAxis(2) * Math.PI) * (Math.PI * 2)
+                             )
+                             .headingWhile(true);
+
+  Command driveFieldOrientedDirectAngleSim = drivebase.driveFieldOriented(driveDirectAngleSim);
+  Command driveSetpointGenSim             = drivebase.driveWithSetpointGeneratorFieldRelative(driveDirectAngleSim);
+
+  // ----------------------------------
+  // Constructor
+  // ----------------------------------
+  public RobotContainer()
+  {
+    // Make sure we bind all the buttons
+    configureBindings();
+
+    // Zero the gyro to correct alliance if needed
+    drivebase.zeroGyro();
+    DriverStation.silenceJoystickConnectionWarning(true);
+
+    NamedCommands.registerCommand("test", Commands.print("I EXIST"));
+  }
+
+  // ----------------------------------
+  // Button/POV bindings
+  // ----------------------------------
+  private void configureBindings()
+  {
+    // Set the default drive command, depending on whether we are in sim or real
+    drivebase.setDefaultCommand(
+        !RobotBase.isSimulation()
+            ? driveFieldOrientedDirectAngle
+            : driveFieldOrientedDirectAngleSim
+    );
+
+    ElavatorSubsystem.setDefaultCommand(new ElavatorCommand(ElavatorSubsystem, .1)); 
+ 
+    //ClawSubsystem.setDefaultCommand(new ClawCommand(ClawSubsystem, .2));
+
+    if (Robot.isSimulation())
+    {
+      driverPs5Controller.options().onTrue(Commands.runOnce(() -> 
+          drivebase.resetOdometry(new Pose2d(3, 3, new Rotation2d())))
+      );
+
+    }
+
+    if (DriverStation.isTest())
+    {
+      drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+
+      driverPs5Controller.triangle().whileTrue(
+          Commands.runOnce(drivebase::lock, drivebase).repeatedly()
+      );
+      driverPs5Controller.square().whileTrue(drivebase.driveToDistanceCommand(1.0, 0.2));
+      driverPs5Controller.options().onTrue((Commands.runOnce(drivebase::zeroGyro)));
+      driverPs5Controller.PS().whileTrue(drivebase.centerModulesCommand());
+    }
+    else
+    {
+      // ------------------
+      // Driver controls
+      // ------------------
+      driverPs5Controller.circle().onTrue(Commands.runOnce(drivebase::zeroGyro));
+      driverPs5Controller.triangle().onTrue(Commands.runOnce(drivebase::addFakeVisionReading));
+      driverPs5Controller.cross().whileTrue(
+          drivebase.driveToPose(
+              new Pose2d(new Translation2d(6.161, 3.905), Rotation2d.fromDegrees(158.806))
+          )
+      );
+      driverPs5Controller.square().whileTrue(drivebase.aimAtSpeaker(2));
+
+      driverPs5Controller.options().whileTrue(Commands.none());
+      driverPs5Controller.PS().whileTrue(Commands.none());
+      driverPs5Controller.L1().whileTrue(
+          Commands.runOnce(drivebase::lock, drivebase).repeatedly()
+      );
+
+      // ------------------
+      // Operator controls
+      // ------------------
+
+
+
+      dpadUpButton.onTrue(new SetElavatorPositionCommand(ElavatorSubsystem, frc.robot.subsystems.ElavatorSubsystem.L1, 1.0));
+      dpadDownButton.onTrue(new SetElavatorPositionCommand(ElavatorSubsystem, frc.robot.subsystems.ElavatorSubsystem.L2, 1.0));
+      dpadRightButton.onTrue(new SetElavatorPositionCommand(ElavatorSubsystem, frc.robot.subsystems.ElavatorSubsystem.L3, 1.0));
+
+
+    }
+  }
+
+
+  // ----------------------------------
+  // Autonomous
+  // ----------------------------------
+  //public Command getAutonomousCommand()
+  {
+    // Example command used for autonomous
+    //return drivebase.getAutonomousCommand("Blue Auto 1");
+  }
+
+  // ----------------------------------
+  // Extra methods
+  // ----------------------------------
+  public void setDriveMode()
+  {
+    configureBindings();
+  }
+
+  public void setMotorBrake(boolean brake)
+  {
+    drivebase.setMotorBrake(brake);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/ElavatorSubsystem.java
@@ -0,0 +1,89 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
+public class ElavatorSubsystem extends SubsystemBase {
+    private final SparkMax elavatorMotor1;
+    private final SparkMax elavatorMotor2;
+    private final RelativeEncoder elavatorEncoder;
+    private final SparkClosedLoopController elavatorPID;
+    private final SparkMaxConfig motorConfig;
+
+    private static final double kP = 0.1;
+    private static final double kI = 0.0;
+    private static final double kD = 0.0;
+    private static final double kMinOutput = -1.0;
+    private static final double kMaxOutput = 1.0;
+    private static final double kConversionFactor = 0.5;
+
+    public static final double L1 = 0.0;
+    public static final double L2 = 20.0;
+    public static final double L3 = 40.0;
+    public static final double L4 = 60.0;
+
+    public ElavatorSubsystem() {
+        // Initialize motors
+        elavatorMotor1 = new SparkMax(50, MotorType.kBrushless);
+        elavatorMotor2 = new SparkMax(51, MotorType.kBrushless);
+
+        // Get encoder from motor 1
+        elavatorEncoder = elavatorMotor1.getEncoder();
+
+        // Create motor configuration object
+        motorConfig = new SparkMaxConfig();
+
+        // Configure encoder conversion factors inside motorConfig
+        motorConfig.encoder
+            .positionConversionFactor(kConversionFactor)
+            .velocityConversionFactor(kConversionFactor);
+
+        // Configure the closed-loop PID controller
+        motorConfig.closedLoop
+            .feedbackSensor(FeedbackSensor.kPrimaryEncoder)  // Use internal encoder
+            .p(kP, ClosedLoopSlot.kSlot0)  // Set PID for position (slot 0)
+            .i(kI, ClosedLoopSlot.kSlot0)
+            .d(kD, ClosedLoopSlot.kSlot0)
+            .outputRange(kMinOutput, kMaxOutput, ClosedLoopSlot.kSlot0);  // Set output range
+
+        // Apply configuration to Spark MAX
+        elavatorMotor1.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
+        // Get PID controller
+        elavatorPID = elavatorMotor1.getClosedLoopController();
+
+        // Reset encoder to 0 at startup
+        elavatorEncoder.setPosition(0.0);
+    }
+
+    public void moveArm(double speed) {
+        elavatorMotor1.set(speed);
+        elavatorMotor2.set(speed);
+    }
+
+    public void stopArm() {
+        elavatorMotor1.set(0.0);
+        elavatorMotor2.set(0.0);
+    }
+
+    public void setPosition(double rotations) {
+        elavatorPID.setReference(rotations, ControlType.kPosition, ClosedLoopSlot.kSlot0);
+    }
+
+    public double getPosition() {
+        return elavatorEncoder.getPosition();
+    }
+
+    public boolean atSetpoint(double target, double tolerance) {
+        return Math.abs(getPosition() - target) <= tolerance;
+    }
+}
